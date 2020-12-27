1035fac72003f485bc006095884f98cc321cff9a
Sergei Shvedko <ew7-21@mail.ru>
2020-12-27 11:29:10 +0300

    Add logger

--- homework.py
+++ homework.py
@@ -49,8 +49,8 @@ def get_homework_statuses(current_timestamp):
         return homework_statuses.json()
     except Exception as e:
         message = f'Бот сломался в методе get_homework_statuses: {e}'
-        # logger.error(
-        #     f'I broke down in get_homework_status!!! my error ====>>>{e}')
+        logger.error(
+            f'I broke down in get_homework_status!!! my error ====>>>{e}')
 
 
 def send_message(message, bot_client):
@@ -61,7 +61,7 @@ def main():
     # проинициализировать бота здесь
     bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())  # начальное значение timestamp
-    # logger.debug('I stared to work')
+    logger.debug('I stared to work')
     send_message('I started to work', bot_client)
     while True:
         try:
@@ -70,14 +70,14 @@ def main():
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(
                     new_homework.get('homeworks')[0]), bot_client)
-                # logger.info('I sent you a message')
+                logger.info('I sent you a message')
             current_timestamp = new_homework.get(
                 'current_date', current_timestamp)  # обновить timestamp
             time.sleep(1200)
 
         except Exception as e:
             message = f'Бот столкнулся с ошибкой: {e}'
-            # logger.error(f'I broke down!!! my error ====>>>{e}')
+            logger.error(f'I broke down!!! my error ====>>>{e}')
             send_message(message, bot_client)
             time.sleep(5)
 
